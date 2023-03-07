0b5549dacc8b56fcf814a80eadefe9195c00e133
guacajoely <guacajoely@gmail.com>
2023-03-07 13:57:08 -0500

    initial commit

---
+++ scripts/BeyBlog.js
@@ -0,0 +1,21 @@
+export const BeyBlog = () => {
+    return `
+        <section class="container">
+        
+            <article class="authors column">
+                <h2>Authors</h2>
+            </article>
+
+            <article class="articles column">
+                <h2>Articles</h2>
+            </article>
+
+        </section>
+
+        <article class="categories column">
+            <h2>Categories</h2>
+        </article>
+        
+      `;
+  };
+  
